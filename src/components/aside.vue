47747d36934b6ea07ba82ba1a8ef73aef777e18e
wangxm47 <1099422647@qq.com>
2020-01-15 21:02:41 +0800

    aside组件

---
+++ src/components/aside.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class='aside'>
+        <h1>阅读</h1>
+        <h3>读万卷书,行万里路</h3>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Aside',
+    }
+</script>
+
+<style scoped="scoped">
+    h1 {
+        font-weight: 700;
+        font-family: Baskerville, "Times New Roman", "Liberation Serif", STFangsong, FangSong, FangSong_GB2312, "CWTEX\-F", serif;
+    }
+
+    h3 {
+        font-family: Baskerville, Georgia, "Liberation Serif", "Kaiti SC", STKaiti, "AR PL UKai CN", "AR PL UKai HK", "AR PL UKai TW", "AR PL UKai TW MBE", "AR PL KaitiM GB", KaiTi, KaiTi_GB2312, DFKai-SB, "TW\-Kai", serif;
+        color: #7b8086;;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="home">
+        <div class = 'aside'>
+            <Aside />
+        </div>
         <div class="books">
             <Book bookName="遮天" bookImageSrc="https://img.zhaishuyuan.com/bookpic/s204.jpg" bookResource="https://www.zhaishuyuan.com/book/204"
                 readChapter="100" />
@@ -12,11 +15,28 @@
 <script>
     // @ is an alias to /src
     import Book from '@/components/Book.vue'
-
+    import Aside from '../components/aside.vue'
     export default {
         name: 'home',
         components: {
-            Book
+            Book,
+            Aside
         }
     }
 </script>
+<style scoped="scoped">
+    .home{
+        display: flex;
+        min-width: 800px;
+        min-height: 500px;
+    }
+    .aside{
+        min-width: 300px;
+        background-color: #f7f7f7;
+        height: 100vh;
+        overflow: hidden;
+    }
+    .books{
+        flex: 1;
+    }
+</style>
