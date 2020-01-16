39c7e5b9529b5ac4ef011aaa7559cb2397336f02
wangxm47 <1099422647@qq.com>
2020-01-16 20:25:39 +0800

    aside组件完善

--- src/components/aside.vue
+++ src/components/aside.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class='aside'>
+    <div class='wrapper'>
         <h1>阅读</h1>
         <h3>读万卷书,行万里路</h3>
-        <div></div>
-        <div></div>
+        <div class = 'search'>
+            <span class="icon line"></span>
+            <input placeholder="搜索书籍" class ='input line' type="text" autocomplete="off">
+        </div>
+        <div class = 'history'>
+            
+        </div>
     </div>
 </template>
 
@@ -14,6 +19,9 @@
 </script>
 
 <style scoped="scoped">
+    .wrapper{
+        height: 100%;
+    }
     h1 {
         font-weight: 700;
         font-family: Baskerville, "Times New Roman", "Liberation Serif", STFangsong, FangSong, FangSong_GB2312, "CWTEX\-F", serif;
@@ -22,5 +30,34 @@
     h3 {
         font-family: Baskerville, Georgia, "Liberation Serif", "Kaiti SC", STKaiti, "AR PL UKai CN", "AR PL UKai HK", "AR PL UKai TW", "AR PL UKai TW MBE", "AR PL KaitiM GB", KaiTi, KaiTi_GB2312, DFKai-SB, "TW\-Kai", serif;
         color: #7b8086;;
+    }
+    .search{
+        margin-top: 35px;
+        position: relative;
+        width: 100%;
+        padding: 0 20px;
+    }
+    .input{
+        border-radius: 50px;
+        border-color: #e3e3e3;
+        border-style: solid;
+        border-width: 1px;
+        padding-left: 30px;
+        padding-right: 15px;
+        outline: none;
+        width: 100%
+    }
+    .line{
+        line-height: 25px;
+        height: 25px;
+    }
+    .icon{
+        position: absolute;
+        left: 25px;
+        top: 0px;
+        text-align: center;
+    }
+    .icon::after{
+        content: '\1F50D';
     }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="home">
-        <div class = 'aside'>
+        <div class = 'aside wrapper-padding'>
             <Aside />
         </div>
-        <div class="books">
+        <div class="books wrapper-padding">
             <Book bookName="遮天" bookImageSrc="https://img.zhaishuyuan.com/bookpic/s204.jpg" bookResource="https://www.zhaishuyuan.com/book/204"
                 readChapter="100" />
             <Book bookName="遮天" bookImageSrc="https://img.zhaishuyuan.com/bookpic/s204.jpg" bookResource="https://www.zhaishuyuan.com/book/204"
@@ -30,8 +30,11 @@
         min-width: 800px;
         min-height: 500px;
     }
+    .wrapper-padding{
+        padding: 25px 20px;
+    }
     .aside{
-        min-width: 300px;
+        min-width: 320px;
         background-color: #f7f7f7;
         height: 100vh;
         overflow: hidden;
