462627fdb8dc2b3e197f8d6e6845bfd7d2bbadb0
wangxm47 <1099422647@qq.com>
2020-01-17 14:58:06 +0800

    aside组件 history部分完善

--- src/components/aside.vue
+++ src/components/aside.vue
@@ -7,8 +7,12 @@
             <input placeholder="搜索书籍" class='input line' type="text" autocomplete="off">
         </div>
         <div class='history-wrapper'>
-            <h5>最近阅读</h5>
+            <h4>最近阅读</h4>
             <div class="history">{{name}}</div>
+        </div>
+        <div class='setting-wrapper'>
+            <h4>基本设定</h4>
+            
         </div>
     </div>
 </template>
@@ -30,11 +34,11 @@
         computed: {
             name() {
                 if (this.bookName == '') {
-                     return "尚无最近阅读记录";
+                    return "尚无最近阅读记录";
                 } else {
                     return this.bookName;
                 }
-                
+
             }
         },
     }
@@ -45,7 +49,8 @@
         height: 100%;
     }
 
-    h1 {
+    h1,
+    h4 {
         font-weight: 700;
         font-family: Baskerville, "Times New Roman", "Liberation Serif", STFangsong, FangSong, FangSong_GB2312, "CWTEX\-F", serif;
     }
@@ -53,7 +58,10 @@
     h3 {
         font-family: Baskerville, Georgia, "Liberation Serif", "Kaiti SC", STKaiti, "AR PL UKai CN", "AR PL UKai HK", "AR PL UKai TW", "AR PL UKai TW MBE", "AR PL KaitiM GB", KaiTi, KaiTi_GB2312, DFKai-SB, "TW\-Kai", serif;
         color: #7b8086;
-        ;
+    }
+
+    h4 {
+        color: #7b8086;
     }
 
     .search {
@@ -61,6 +69,27 @@
         position: relative;
         width: 100%;
         padding: 0 20px;
+    }
+    .history-wrapper{
+        margin-top: 40px;
+        width: 100%;
+        padding: 15px 15px;
+        text-align: left;
+        border-top: 1px #e3e3e3 solid;
+        border-bottom: 1px #e3e3e3 solid;
+    }
+    .setting-wrapper{
+        width: 100%;
+        text-align: left;
+        padding: 15px 15px;
+    }
+    .history{
+        padding: 8px 40px;
+        font-weight: 800;
+        font-size: 14px;
+    }
+    .history:hover{
+        background-color: #e1e0e0;
     }
 
     .input {
